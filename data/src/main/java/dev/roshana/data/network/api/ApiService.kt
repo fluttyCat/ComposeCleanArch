80e53699ba3cbc5dd2b7ff665e5bae6eed69b18d
PariSa <parisahazhirghader@gmail.com>
2022-08-29 14:50:07 +0430

    domain and data module created, api service interface created.

---
+++ data/src/main/java/dev/roshana/data/network/api/ApiService.kt
@@ -0,0 +1,4 @@
+package dev.roshana.data.network.api
+
+interface ApiService {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,5 @@ dependencyResolutionManagement {
 rootProject.name = "JetpackComposeBaseArch"
 include(":app")
 
+include(":data")
+include(":domain")
