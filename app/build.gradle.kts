39a42713f99810a2d618c2f680c06463ba297fc9
PariSa <parisahazhirghader@gmail.com>
2022-08-29 15:01:21 +0430

    kotlin version changed because of incompatibility.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-android")
-    id("kotlin-android-extensions")
+    //id("kotlin-android-extensions")
     id("kotlin-kapt")
 }
 
