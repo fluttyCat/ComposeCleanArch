f24431cf5e54c08487af98626b1420ebd0dc27b6
PariSa <parisahazhirghader@gmail.com>
2022-08-31 10:37:23 +0430

    hilt anotation added to main activity.

--- app/src/main/java/dev/roshana/jetpackcomposebasearch/MainActivity.kt
+++ app/src/main/java/dev/roshana/jetpackcomposebasearch/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.roshana.jetpackcomposebasearch.ui.theme.JetpackComposeBaseArchTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/dev/roshana/jetpackcomposebasearch/navigation/NavigationItem.kt
+++ app/src/main/java/dev/roshana/jetpackcomposebasearch/navigation/NavigationItem.kt
@@ -2,6 +2,6 @@ package dev.roshana.jetpackcomposebasearch.navigation
 
 import dev.roshana.jetpackcomposebasearch.R
 
-sealed class NavigationItem(var title: String, var icon: Int, var screen_route: String) {
-    object Article : NavigationItem("article", R.drawable.ic_baseline_face,"Article")
+sealed class NavigationItem(var title: String, var screen_route: String) {
+    object Article : NavigationItem("article", "Article")
 }
