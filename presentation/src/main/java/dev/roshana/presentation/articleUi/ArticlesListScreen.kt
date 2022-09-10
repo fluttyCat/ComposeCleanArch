8303258e9cc20f898bc3097f13266eba5e971bd2
PariSa <parisahazhirghader@gmail.com>
2022-09-10 15:07:24 +0430

    top bar sample added.

--- presentation/src/main/java/dev/roshana/presentation/articleUi/ArticlesListScreen.kt
+++ presentation/src/main/java/dev/roshana/presentation/articleUi/ArticlesListScreen.kt
@@ -1,17 +1,24 @@
 package dev.roshana.presentation.articleUi
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.min
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -31,15 +38,30 @@ fun ArticlesListScreen() {
     val showColumn by remember {
         mutableStateOf(true)
     }
-
     val scope = rememberCoroutineScope()
+    val toolBarHeight by animateDpAsState(
+        targetValue = min(56.dp, 100.dp),
+    )
+
 
     val articles = state.dataList?.collectAsLazyPagingItems()
-    Scaffold {
+    Scaffold(topBar = {
+        TopAppBar(
+            modifier = Modifier
+                .height(toolBarHeight),
+            title = {
+                Text(
+                    text = "Articles",
+                    color = Color.Black
+                )
+            },
+            backgroundColor = Color.White,
+        )
+    }) {
+
         Box(modifier = Modifier.padding(it)) {
 
             if (state.errorMessage.isNotEmpty()) {
-
                 Box(
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
@@ -50,10 +72,10 @@ fun ArticlesListScreen() {
                 articles?.let { items ->
 
                     if (showColumn) {
-                    ArticlesListColumn(items = items, listState = lazyListState) { articleId ->
+                        ArticlesListColumn(items = items, listState = lazyListState) { articleId ->
 
+                        }
                     }
-                }
                 }
             }
         }
